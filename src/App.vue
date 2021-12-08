7477c070280c4e65e1c1a408416850dd2ef6ea89
imkf-zhang <15638452918@163.com>
2021-12-08 18:12:50 +0800

    [功能]创建vue应用实例

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div class="container">
+      根组件
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: 'App'
+};
 </script>
+<style lang='less' scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
+// 以前main.js的作用是创建一个根实例，现在是创建一个vue应用
 
-createApp(App).mount('#app')
+// 1、导入createApp函数
+import { createApp } from "vue"
+
+// 2、 编写一个根组件App.vue，导入进来
+import App from './App.vue'
+
+// 3、基于根组件创建应用实例
+const app = createApp(App)
+// 4、挂载到index.html的#app容器
+app.mount('#app')
