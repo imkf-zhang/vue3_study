24186409c3f49989ed609330aa6285f3e328716d
imkf-zhang <15638452918@163.com>
2021-12-09 15:32:05 +0800

    [study]组合api--数据双向绑定(reactive函数的使用)

---
+++ src/03、reactive函数(双向绑定).vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    根组件
+    <div>{{obj.name}}</div>
+    <div>{{obj.age}}</div>
+    <button @click="updataName">修改数据</button>
+  </div>
+</template>
+<script>
+import { onBeforeMount, onMounted, reactive } from "vue";
+export default {
+  name: "App",
+  setup() {
+    // reactive是一个函数，可以定义一个比较复杂的数据类型，成为响应式数据
+    const obj = reactive({
+      name: "ls",
+      age: 18,
+    });
+    // 修改名字
+    const updataName = () => {
+        console.log("修改名字函数运行")
+        obj.name = 'zs'
+    }
+    return { obj, updataName }
+  },
+};
+</script>
+<style lang='less' scoped>
+</style>
