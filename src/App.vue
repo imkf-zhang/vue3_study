2260a7048ee9a720827d70e0cbe0d3c608bdf386
imkf-zhang <15638452918@163.com>
2021-12-09 16:03:05 +0800

    [study]组合api--toRef，toRef是函数，转换响应式对象中某个属性为单独响应式数据，并且值是关联的

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    根组件
+   <div><i>名字：</i>{{name}}</div>
+   <button @click="updataName">修改</button>
+  </div>
+</template>
+<script>
+import { onBeforeMount, onMounted, reactive,toRef } from "vue";
+export default {
+  name: "App",
+  setup() {
+    // 1、响应式数据对象
+    const obj = reactive({
+        name: 'ls',
+        age: 10
+    });
+    // 2、模板只需要使用name
+    const name = toRef(obj,'name');
+    // 3、方法
+    const updataName = () => {
+        console.log('name',name)
+        name.value = "zs"
+    }
+
+    return { name,updataName}
+  },
+};
+</script>
+<style lang='less' scoped>
+</style>
