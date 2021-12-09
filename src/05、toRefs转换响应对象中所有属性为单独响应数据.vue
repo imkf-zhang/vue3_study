96e4f15fc1773be9e1ba8b9ba3f77c201b45380e
imkf-zhang <15638452918@163.com>
2021-12-09 17:03:06 +0800

    [study]组合api--toRefs,转换响应式对象中所有属性为单独响应式数据

---
+++ src/05、toRefs转换响应对象中所有属性为单独响应数据.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    根组件
+   <div>{{name}}</div>
+   <div>{{age}}</div>
+   <button @click="updataName">修改数据</button>
+  </div>
+</template>
+<script>
+import { reactive,toRefs } from "vue";
+export default {
+  name: "App",
+  setup() {
+    // 1、响应式数据对象
+    const obj = reactive({
+        name: 'ls',
+        age: 10
+    });
+    console.log(obj)
+    
+    // 2、toRefs
+    const obj2 = toRefs(obj)
+    console.log(obj2)
+    // 方法
+    const updataName = () => {
+        obj2.name.value = "zhangsan"
+        // 也可以写成如下代码：他们是相通的，值是关联的
+        // obj.name = 'zs'
+    }
+    return { ...obj2,updataName }
+  },
+};
+</script>
+<style lang='less' scoped>
+</style>
