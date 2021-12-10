4a210326c3cfef532e00e7eb8e5b61b00fc9d820
imkf-zhang <15638452918@163.com>
2021-12-10 14:33:38 +0800

    [study]组合式api-computed的简单使用-函数

---
+++ src/09、computed的简单使用.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="container">
+      <h1>年龄</h1>
+      <div>今年：{{age}}</div>
+      <div>后年：{{newAge}}</div>
+      <hr />
+      <h1>体重</h1>
+      <div>前：{{yearOld}}</div>
+      <div>后：{{yearNew}}</div>
+  </div>
+</template>
+<script>
+import { ref, computed} from 'vue'
+export default {
+  name: "App",
+  setup() {
+    // 1、计算属性：需要依赖现有响应式数据，根据一定的逻辑得到一个新的数据
+    const age = ref(6)
+    // 2、必须要return，这个返回值就是计算属性的值
+    const newAge = computed(() => {
+        return age.value + 2
+    })
+
+    let yearOld = ref(148)
+    const yearNew = computed({
+        get() {
+            return yearOld.value - 8
+        },
+        set(value) {
+            yearOld.value = value + 8
+        }
+    })
+    return { age, newAge, yearOld, yearNew};
+  },
+};
+</script>
+<style lang='less' scoped>
+</style>
