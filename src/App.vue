a8a6cc58589a84662fb1c91b1f2c319d35df3da6
imkf-zhang <15638452918@163.com>
2021-12-13 13:56:36 +0800

    [study]数据传值--子传父

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="container">
+    <h1>父组件</h1>
+    <div>{{ money }}</div>
+    <hr />
+    <son-two :mon="money" @change-money="unpdataMoney"></son-two>
+  </div>
+</template>
+<script>
+import { ref } from "vue";
+import SonTwo from "./SonTwo.vue";
+export default {
+  name: "App",
+  components: {
+    SonTwo,
+  },
+  setup() {
+    const money = ref(101);
+    const unpdataMoney = (newMoney) => {
+      money.value = money.value- newMoney;
+    };
+    return { money, unpdataMoney };
+  },
+};
+</script>
+<style lang='less' scoped>
+</style>
---
+++ src/SonTwo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="container">
+      <h3>子组件</h3>
+      <div>子组件内容:{{mon}}</div>
+      <button @click="changeMoney">-1按钮</button>
+  </div>
+</template>
+<script>
+import { onMounted } from 'vue'
+export default {
+  name: "SonTwo",
+  props: {
+    mon: {
+      type: Number,
+      default: 0
+    }
+  },
+  /**
+   * props 父组件数据
+   * emit 触发自定义事件函数
+   */
+  setup(props,context) {
+    // 获取父组件的数据money
+    console.log(props.mon)
+    console.log(context)
+    // 向父组件传值
+    const changeMoney = () => {
+        context.emit('change-money',1)
+    }
+    return { changeMoney};
+  },
+};
+</script>
+<style lang='less' scoped>
+</style>
